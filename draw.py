fb6ef339eddefe5cb837ad96067a2c5319b1fd4a
lampysprites <lampysprites@users.noreply.github.com>
2022-01-12 14:42:40 +0700

    Add missing license header

--- draw.py
+++ draw.py
@@ -1,4 +1,23 @@
-from os import name
+# Copyright (c) 2021 lampysprites
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 import bpy
 import gpu
 from bpy_extras import view3d_utils
@@ -299,6 +318,8 @@ class SB_WT_Pencil(bpy.types.WorkSpaceTool):
     bl_label = "Pencil"
     bl_description = "Draw image pixels"
     bl_keymap = (
+        # ("", {"type": 'MOUSEMOVE', "value": 'ANY'}, None),
+        ("pribambase.report", {"type": 'Z', "value": 'PRESS', "ctrl": True}, {"properties": [("message", "asdf"), ("message_type", 'INFO')]}),
         ("pribambase.draw_with_pencil", {"type": 'LEFTMOUSE', "value": 'PRESS'}, None),
         ("pribambase.draw_with_pencil", {"type": 'RIGHTMOUSE', "value": 'PRESS'}, {"properties": [("draw_with_bg", True)]}))
 
